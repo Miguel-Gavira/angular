282d67f42dfadb42310a460c442aa7f9a38e9f66
Miguel Gavira <miguel.gavira@lleego.com>
2021-10-09 12:07:07 +0200

    remove files

--- src/app/modules/application.routing.ts
+++
@@ -1,6 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
-
-const routes: Routes = [{ path: '', component: LayoutComponent }];
-
-export const ApplicationRoutes = RouterModule.forChild(routes);
